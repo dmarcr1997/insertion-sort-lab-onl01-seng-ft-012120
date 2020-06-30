deff1561fcab4529969fa1e4cbf834798bb27e5a
dmarc <dmarcr1997@gmail.com>
2020-06-29 19:45:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+    let minNum = array[0]
+    let aryIndex = 0
+    for(let i = 0; i < array.length; i++){
+        if(array[i] < minNum) {
+            minNum = array[i]
+            aryIndex = i
+        }
+    }
+    array.splice(aryIndex, 1)
+    return minNum
 }
 
 function insertionSort(array){
-
+    let sort = []
+    while(array.length !== 0){
+       sort.push(findMinAndRemove(array)) 
+    }
+    return sort
 }
